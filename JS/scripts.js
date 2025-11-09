739333be37a0aded6c973e154de32487d24166e2
projetomvp15-blip <projetomvp15@gmail.com>
2025-11-09 15:29:24 -0300

    .

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,24 +1,16 @@
-/* ===============================================
-   SCRIPT DAS ABAS (Tabs)
-   Usado em: parque.html, petp.html, pnmmt.html
-   ===============================================
-*/
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
-    // Esconde todo o conteúdo das abas
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Remove a classe "active" de todos os botões de aba
     tablinks = document.getElementsByClassName("tab-link");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Mostra a aba clicada e adiciona a classe "active" ao botão
     var tabToShow = document.getElementById(tabName);
     if (tabToShow) {
         tabToShow.style.display = "block";
@@ -27,9 +19,8 @@ function openTab(evt, tabName) {
     if (evt && evt.currentTarget) {
         evt.currentTarget.className += " active";
     }
-}
-    // Ativa a primeira aba por padrão (se houver abas na página)
     var firstTabButton = document.querySelector(".tab-link");
     if (firstTabButton) {
         firstTabButton.click();
     }
+}
